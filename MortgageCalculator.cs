0d15b95640417b96c03f5bb675b3617383bb112b
Scottie Cornett <scottie.cornett@gmail.com>
2022-10-29 18:41:17 -0400

    mortgage calculator logic added

--- MortgageCalculator.cs
+++ MortgageCalculator.cs
@@ -8,5 +8,39 @@ namespace FinalProject
 {
     public class MortgageCalculator
     {
+        private int principal;
+        private float annualInterest;
+        private byte years;
+        private const byte MONTHS_IN_YEAR = 12;
+        private const byte PERCENT = 100;
+
+        public MortgageCalculator(int principal, float annualInterest, byte years)
+        {
+            this.principal = principal;
+            this.annualInterest = annualInterest;
+            this.years = years;
+        }
+
+        public double CalculateBalance(short numberOfPaymentsMade)
+        {
+            float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            float numberOfPayments = years * MONTHS_IN_YEAR;
+
+            double balance = principal
+                * (Math.Pow(1 + monthlyInterest, numberOfPayments) - Math.Pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.Pow(1 + monthlyInterest, numberOfPayments) - 1);
+            return balance;
+        }
+        public double CalculateMortgage()
+        {
+            int numberOfPayments = years * MONTHS_IN_YEAR;
+            float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+            double mortgage = principal
+                * (monthlyInterest * Math.Pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.Pow(1 + monthlyInterest, numberOfPayments) - 1);
+            return mortgage;
+        }
+
     }
 }
